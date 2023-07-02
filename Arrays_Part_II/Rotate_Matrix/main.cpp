5e7df288d192b627e9c8e636024d26d90b569b7b
Anish Gupta <anishgupta675@gmail.com>
2023-07-02 15:07:09 +0530

    Rotate Matrix

---
+++ Arrays_Part_II/Rotate_Matrix/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i = 0; i < matrix.size(); i++) for(int j = i; j < matrix.size(); j++) swap(matrix[i][j], matrix[j][i]);
+        for(int i = 0; i < matrix.size(); i++) for(int j = 0; j < matrix.size() / 2; j++) swap(matrix[i][j], matrix[i][matrix.size() - 1 - j]);
+    }
+};
+
+int main() {}
