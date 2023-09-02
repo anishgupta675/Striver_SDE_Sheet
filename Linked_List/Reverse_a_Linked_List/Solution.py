14121694dc8646c470b837b30a2a3b20196dcba9
Anish Gupta <anishgupta675@gmail.com>
2023-09-02 12:19:17 +0530

    Reverse a Linked List

---
+++ Linked_List/Reverse_a_Linked_List/Solution.py
@@ -0,0 +1,21 @@
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        prev, next = None, head.next
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
