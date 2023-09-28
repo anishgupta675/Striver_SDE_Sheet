aaa7cfdbf65c3fc9139eba1543c69b83137f3b7d
Anish Gupta <anishgupta675@gmail.com>
2023-09-29 00:03:06 +0530

    Add Two Numbers

--- Linked_List/Add_Two_Numbers/main.cpp
+++ Linked_List/Add_Two_Numbers/main.cpp
@@ -1,1 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode *dummyHead = new ListNode(0), *p = l1, *q = l2, *curr = dummyHead;
+        while(p || q) {
+            int x = p ? p->val : 0, y = q ? q->val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            if(p) p = p->next;
+            if(q) q = q->next;
+        }
+        if(carry > 0) curr->next = new ListNode(carry);
+        return dummyHead->next;
+    }
+};
+
 int main() {}
