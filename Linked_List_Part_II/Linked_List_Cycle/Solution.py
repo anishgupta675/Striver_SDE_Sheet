25aa7efd7d9bfe95e7a2dac6b018d9b1de7280a5
Anish Gupta <anishgupta675@gmail.com>
2023-10-03 21:46:50 +0530

    Linked List Cycle

---
+++ Linked_List_Part_II/Linked_List_Cycle/Solution.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+        slow_ptr, fast_ptr = head, head
+        while fast_ptr and fast_ptr.next:
+            fast_ptr = fast_ptr.next.next
+            slow_ptr = slow_ptr.next
+            if slow_ptr == fast_ptr:
+                return True
+        return False
