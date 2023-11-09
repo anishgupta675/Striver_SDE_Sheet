df492dcb7b78e317926466658e7a54b49628052a
Anish Gupta <anishgupta675@gmail.com>
2023-11-09 23:25:44 +0530

    Remove Duplicates from Sorted Array

---
+++ Linked_List_and_Arrays/Remove_Duplicates_from_Sorted_Array/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i++) {
+            if(i < nums.size() && nums[i - 1] == nums[i]) {
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
+
+int main() {}
