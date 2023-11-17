ac87b8e3f950ba60d3bf7be8f0438d38578e3131
Anish Gupta <anishgupta675@gmail.com>
2023-11-17 23:43:06 +0530

    Fractional Knapsack Problem

---
+++ Greedy_Algorithm/Fractional_Knapsack_Problem/Solution.py
@@ -0,0 +1,52 @@
+#User function Template for python3
+
+class Item:
+    def __init__(self,val,w):
+        self.value = val
+        self.weight = w
+        
+class Solution:    
+    #Function to get the maximum total value in the knapsack.
+    def fractionalknapsack(self, W,arr,n):
+        
+        # code here
+        res = 0.0
+        value = [0] * n
+        for i in range(n):
+            value[i] = arr[i].value / arr[i].weight
+        value.sort()
+        for i in range(n - 1, -1, -1):
+            if W <= 0:
+                break
+            res += arr[i].value * min(max(W, 0), arr[i].weight)
+            W -= arr[i].weight
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+class Item:
+    def __init__(self,val,w):
+        self.value = val
+        self.weight = w
+        
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,W = map(int,input().strip().split())
+        info = list(map(int,input().strip().split()))
+        arr = [Item(0,0) for i in range(n)]
+        for i in range(n):
+            arr[i].value = info[2*i]
+            arr[i].weight = info[2*i+1]
+            
+        ob=Solution()
+        print("%.2f" %ob.fractionalknapsack(W,arr,n))
+
+# } Driver Code Ends
