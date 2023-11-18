0799faacdad6e4acdd22404e32573bc553be3fb8
Anish Gupta <anishgupta675@gmail.com>
2023-11-18 22:00:14 +0530

    Activity Selection

---
+++ Greedy_Algorithm/Activity_Selection/main.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+struct meeting {
+   int start;
+   int end;
+   int pos;
+};
+
+class Solution
+{
+    bool static comparator(struct meeting m1, meeting m2) {
+        if (m1.end < m2.end) return true;
+        else if (m1.end > m2.end) return false;
+        else if (m1.pos < m2.pos) return true;
+        return false;
+    }
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int s[], int e[], int n)
+    {
+        // Your code here+
+        struct meeting meet[n];
+        for (int i = 0; i < n; i++) meet[i].start = s[i], meet[i].end = e[i], meet[i].pos = i + 1;
+        sort(meet, meet + n, comparator);
+        int res = 1;
+        int limit = meet[0].end;
+        for (int i = 1; i < n; i++) {
+            if (meet[i].start > limit) {
+                limit = meet[i].end;
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
