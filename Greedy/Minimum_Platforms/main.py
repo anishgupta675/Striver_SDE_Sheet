5f84d410d61a9be01b339931663bba618b1e2f43
Anish Gupta <anishgupta675@gmail.com>
2024-12-20 17:47:49 +0000

    Minimum Platforms

---
+++ Greedy/Minimum_Platforms/main.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to find the minimum number of platforms required at the
+    #railway station such that no train waits.
+    def minimumPlatform(self,arr,dep):
+        # code here
+        arr.sort()
+        dep.sort()
+        i, j, res = 0, 1, 1
+        while j < len(arr):
+            if arr[j] <= dep[i]:
+                res += 1
+            else:
+                i += 1
+            j += 1
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        arrival = list(map(int, input().strip().split()))
+        departure = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.minimumPlatform(arrival, departure))
+        print("~")
+
+# } Driver Code Ends
