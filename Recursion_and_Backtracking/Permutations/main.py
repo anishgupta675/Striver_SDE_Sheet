41c4b303bcb971f9345cdccf88ebc43ed40b32f8
Anish Gupta <anishgupta675@gmail.com>
2024-12-31 18:29:05 +0000

    Permutations

---
+++ Recursion_and_Backtracking/Permutations/main.py
@@ -0,0 +1,20 @@
+class Solution {
+    vector<vector<int>> res;
+    void dfs(vector<int> &nums, vector<int> &temp) {
+        if(temp.size() == nums.size()) res.push_back(temp);
+        else {
+            for(int i = 0; i < nums.size(); i++) {
+                if(find(temp.begin(), temp.end(), nums[i]) != temp.end()) continue;
+                temp.push_back(nums[i]);
+                dfs(nums, temp);
+                temp.erase(temp.begin() + temp.size() - 1);
+            }
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> temp;
+        dfs(nums, temp);
+        return res;
+    }
+};
